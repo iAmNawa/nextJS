5b684255543118d404bc016e974ee12cce5040e6
Paul Borawski <paulborawski14@gmail.com>
2019-04-04 12:03:23 -0700

    link works with anything

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 // This is the Link API
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Index = () => (
   <div>
     <Link href="/about">
-      <a>About Page</a>
+      <button>Go to About Page</button>
     </Link>
     <p>Hello Next.js</p>
   </div>
